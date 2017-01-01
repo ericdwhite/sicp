ea574531d2aaf838e522022c2b5cf1d4f71399d5
Eric D. White <eric@ericwhite.ca>
2017-01-01 19:45:32 +0000

    1.1.7 and 1.3.1

--- src/sicp/core.clj
+++ src/sicp/core.clj
@@ -1,6 +1,45 @@
-(ns sicp.core)
+(ns sicp.core
+  (:require [clojure.math.numeric-tower :as math]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;
+; 1.1.7 Square Roots by Newton's Method
+;
+(defn square [x]
+  (* x x))
+
+(defn sqrt [x]
+  (sqrt-iter 1 x))
+
+(defn average [a b]
+  (/ (+ a b) 2))
+
+(defn improve [guess x]
+  (average guess (/ x guess)))
+
+(defn sqrt-iter [guess x]
+  (letfn [(good-enough? [guess x]
+            (<= (math/abs (- (square guess) x)) 0.001))]
+    (if (good-enough? guess x)
+      guess
+      (recur (improve guess x)
+             x))))
+
+;
+; 1.3.1 Higher Order Procedures
+;
+(defn cube [x]
+  (* x x x))
+
+(defn sum [term a step b]
+  (if (> a b)
+    0
+    (+ (term a)
+       (sum term (step a) step b))))
+
+(defn sum-integers [a b]
+  (sum identity a inc b))
+
+(defn sum-cubes [a b]
+  (sum cube a inc b))
+
+
--- test/sicp/core_test.clj
+++ test/sicp/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [sicp.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+; SICP tests
+;
+;    (sicp.core-test/test-sicp)
+;
+(defn test-sicp []
+  (run-tests 'sicp.core-test))
+
+(deftest test-1-1-7
+  (testing "Square Root by Netwon's Method"
+    (is (= 2.0000000929222947 (sqrt 4.0)))))
+
+(deftest test-1-3-1
+  (testing "Higher Order Procedures"
+    (is (= 6 (sum-integers 1 3)))
+    (is (= 36 (sum-cubes 1 3)))))

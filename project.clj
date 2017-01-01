daf99bd255b92749d84a96fa7f128b868a676e05
Eric D. White <eric@ericwhite.ca>
2017-01-01 16:32:15 +0000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject sicp "0.1.0-SNAPSHOT"
+  :description "SICP"
+  :url "http://github.com/ericdwhite/sicp"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
